bfaa0367227a8b3b07ddc5f24cf6b3e29d2b999a
J Adom <jordanaharris@gmail.com>
2019-08-24 22:49:29 -0500

    adding readme file

--- TriviaGame/assets/javascript/app.js
+++ TriviaGame/assets/javascript/app.js
@@ -7,8 +7,10 @@
 // 
 
 let guessTime = 10;
+let breakTime = 5
 let questionCount = 0;
 let intervalId;
+let breakID
 let wrongCount = 0;
 let rightCount = 0;
 
@@ -76,6 +78,7 @@ const questionSet = [q1,q2,q3,q4,q5,q6,q7,q8,q9,q10];
 
 let welcome = "Click 'Play the Game' to start. You have "+ guessTime + " seconds to answer per question and there are "+ questionSet.length + " questions"
 
+// Creates starting page on file open
 function gameIntro(){
     $("#count-timer").html("<h2>" + welcome + "</h2>")
     
@@ -87,9 +90,18 @@ function countdown(){
     $("#count-timer").html("<h2>You have "+ guessTime + " seconds left!")
     guessTime--;
     if (guessTime === -1){
+        questionCount += 1
         wrongCount++
-        console.log(wrongCount)
-        gameStatus()
+        
+        guessTime = 10;
+        breakTime = 5
+        questionCount++
+        
+        $("#count-timer").html("Tally: "+ rightCount +" Right -" +wrongCount +" Wrong")
+        $("quiz-box").html("")
+        clearInterval(breakID);
+        breakID = setInterval(timedBreak, 1000);
+        nextQuestion()
     }
 
 }
@@ -99,54 +111,65 @@ function nextQuestion(){
         endGame();
     }
     else{
-        console.log(questionSet)
-        let currentQuestion = questionSet[questionCount] 
-        console.log(currentQuestion)
-        let textQuestion = $("<p>").text(currentQuestion.question)
-        console.log(currentQuestion.question)
-        let textAns1 = $("<div>").text(currentQuestion.answer1[0])
-            .addClass("btn btn-secondary btn-lg btn-block")       
-            .attr("id","answer-1 ")
-            .attr("value", currentQuestion.answer1[1])
-        let textAns2 = $("<div>").text(currentQuestion.answer2[0])
-            .addClass("btn btn-secondary btn-lg btn-block")
-            .attr("id","answer-2 ")
-            .attr("value", currentQuestion.answer2[1])
-        let textAns3 = $("<div>").text(currentQuestion.answer3[0])
-            .addClass("btn btn-secondary btn-lg btn-block")
-            .attr("id","answer-3 ")
-            .attr("value", currentQuestion.answer3[1])
-        
-        $("#quiz-box").text("")
-        $("#quiz-box").append(textQuestion)
-        $("#quiz-box").append(textAns1)
-        $("#quiz-box").append(textAns2)
-        $("#quiz-box").append(textAns3)
+            console.log(questionSet)
+            let currentQuestion = questionSet[questionCount] 
+            console.log(currentQuestion)
+            let textQuestion = $("<p>").text(currentQuestion.question)
+            console.log(currentQuestion.question)
+            let textAns1 = $("<div>").text(currentQuestion.answer1[0])
+                .addClass("btn btn-secondary btn-lg btn-block")       
+                .attr("id","answer-1 ")
+                .attr("value", currentQuestion.answer1[1])
+            let textAns2 = $("<div>").text(currentQuestion.answer2[0])
+                .addClass("btn btn-secondary btn-lg btn-block")
+                .attr("id","answer-2 ")
+                .attr("value", currentQuestion.answer2[1])
+            let textAns3 = $("<div>").text(currentQuestion.answer3[0])
+                .addClass("btn btn-secondary btn-lg btn-block")
+                .attr("id","answer-3 ")
+                .attr("value", currentQuestion.answer3[1])
+            console.log(currentQuestion.answer1[1])
+            $("#quiz-box").text("")
+            $("#quiz-box").append(textQuestion)
+            $("#quiz-box").append(textAns1)
+            $("#quiz-box").append(textAns2)
+            $("#quiz-box").append(textAns3)
 
-        clearInterval(intervalId);
-        intervalId = setInterval(countdown, 1000);
+            // Start countdown clock
+            clearInterval(intervalId);
+            intervalId = setInterval(countdown, 1000);
 
-        $(".btn").click(function(){
-            var userGuess = $(this).val()
-            console.log(questionCount)
-            console.log(userGuess)
-            if(userGuess === true){
-                rightCount++
-            } else{
-                wrongCount++
-            }
-            setTimeout(() => { gameStatus()}, 5000); 
-        })
+            $(".btn").click(function(){
+                console.log($(this).attr("value"))
+                var userGuess = $(this).attr("value")
+                
+                console.log(questionCount)
 
-        
+                console.log(userGuess)
+                if(userGuess === "true"){
+                    rightCount++
+                } else{
+                    wrongCount++
+                }
+                console.log(rightCount + " " + wrongCount)
+                guessTime = 10;
+                breakTime = 5
+                
+                
+               
+            })
+
+            
+        }
+
     }
-
-}
 function endGame(){
     $("#count-timer").html("<h2>Game Over!</h2>")
         .append("<h2>You got "+ rightCount + " questions right!</h2>")
     $("#play-button").text("Start Over?")
-    $("#play-button").on("click", function(){
+    
+    // Starts game from the beginning if clicked    
+    $(document).on("click", "#play-button", function(){
         $("#play-button").text("Start Over?")
         $("#count-timer").text("")
         guessTime = 10;
@@ -161,29 +184,27 @@ function endGame(){
    
 }
 
-function gameOn(){
 
-
-
-}
-
-
-
+// Shows current score between questions
 function gameStatus(){
-    guessTime = 10;
     questionCount++
-    
-    $("#count-timer").text("Tally: "+ rightCount +" Right -" +wrongCount +" Wrong")
-    $("quiz-box").text("")
-
-
+    $("#count-timer").html("Tally: "+ rightCount +" Right -" +wrongCount +" Wrong")
+    $("quiz-box").html("")
+    clearInterval(breakID);
+    breakID = setInterval(timedBreak, 1000);
+    nextQuestion()
 }
-function startGame(){
+
+function timedBreak(){
+    breakTime--;
+    if (breakTime === -1){
+        return;
+    }
 }
 
 gameIntro();   
 
-$("#play-button").on("click", function(){
+$(document).on("click", "#play-button", function(){
     $("#count-timer").text("")
     
     nextQuestion();
